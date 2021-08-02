049aff7a416a7860dd08e2e81a057c6f754a80d0
Abdikarim-Ahmed <88340466+Abdikarim-Ahmed@users.noreply.github.com>
2021-08-02 20:44:51 +0300

    Add files via upload
    data structure and function prototypes

---
+++ main.c
@@ -0,0 +1,33 @@
+/*-----------------------------------
+ Project:  Library Management System
+ Author:   Abdikarim Ahmed
+ Date:     August, 2021
+ Compiler: C99
+ License:  MIT
+-----------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <windows.h>
+#include <conio.h>
+
+// Data structures
+
+struct user {
+    int id;
+    char name[100];
+    char tel[13];
+    int is_staff;
+};
+
+// function prototypes
+void execute_action(int action);
+void close();
+int menu();
+void add_user();
+void view_users();
+
+int main()
+{
