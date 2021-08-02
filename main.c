ce4191104e3ed8c6928af387c2dd695f5ac22b2b
Abdikarim-Ahmed <88340466+Abdikarim-Ahmed@users.noreply.github.com>
2021-08-02 20:51:56 +0300

    Add files via upload
    navigation menu and validation

--- main.c
+++ main.c
@@ -31,3 +31,60 @@ void view_users();
 
 int main()
 {
+int action;
+    printf("\tCounty Library Management System!\n");
+    printf("Welcome Abdikarim\n");
+    // Navigation menu
+    do {
+        printf("1. Add user\n");
+        printf("2. Add Book\n");
+        printf("3. Exit\n");
+        printf("Select action(1-3): ");
+        scanf("%d",&action);
+        // validate input
+        if (action < 1 || action > 3)
+            printf("Invalid action. Try again\n");
+    } while(action < 1 || action > 3);
+    int action;
+    while(1) {
+        action = menu();
+        execute_action(action);
+        printf("Press any key to continue");
+        getchar();
+        getch();
+        system("cls");
+    }
+
+    return 0;
+}
+
+
+
+void execute_action(int action) {
+    switch (action) {
+    case 1:
+        add_user();
+        break;
+    case 2:
+        view_users();
+        break;
+    case 3:
+        printf("adding book\n");
+        break;
+    case 4:
+        close();
+        break;
+    default:
+        printf("Sorry. I don't know how to execute that\n");
+    }
+
+}
+
+void close() {
+    printf("Thank for you using the system.");
+    Sleep(5000);
+    exit(0);
+}
+
+int menu() {
+    int action;
